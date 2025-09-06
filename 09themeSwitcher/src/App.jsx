5b5ef9c8915a6af81ffcd2256a07b094b3d8790b
Ayush <ayushstillworks@gmail.com>
2025-09-06 12:59:29 +0530

    made a theme switcher button using a different and more optimized syntax for the context api.
    here's how it is different from 09miniContext code
    
    - wrote themeContext , theme context provider in same file
    
    - stored themeContext.provider in a single variable
    
    -made a function called useTheme that returns useContext(themeContext) so i wont have to write it everytime

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import ThemeBtn from './components/themeBtn'
+import Card from './components/cards'
+import './App.css'
+import { ThemeProvider } from './context/theme'
+
+function App() {
+  const [themeMode, setThemeMode] = useState(0)
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+
+  //actual change in theme
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
+   
+  
+  
+  
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
